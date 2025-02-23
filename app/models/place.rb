9b7efa0384c0006770a16f2066a075553c90e24c
Megha Kumar <megha.kumar@kellogg.northwestern.edu>
2025-02-23 03:29:45 +0000

    hw

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,4 @@
-class Place < ApplicationRecord
-  # talks to the "places" table
-end
+
+  class Place < ApplicationRecord
+    has_many :entries, dependent: :destroy
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+Rails.application.routes.draw do
   # Defines the root path route ("/")
-  # get("/", { :controller => "articles", :action => "index" })
+  root "places#index"
+
+  resources :places do
+    resources :entries, only: [:new, :create]
+  end
 end
+
